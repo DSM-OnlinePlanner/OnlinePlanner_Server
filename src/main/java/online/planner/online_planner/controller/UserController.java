b0e95f6cfb415cfbf600f97608404efdeeec1719
서건희 <seogunhee4@gmail.com>
2021-09-01 22:57:57 +0900

    [FIX]delete account

--- src/main/java/online/planner/online_planner/controller/UserController.java
+++ src/main/java/online/planner/online_planner/controller/UserController.java
@@ -1,6 +1,7 @@
 package online.planner.online_planner.controller;
 
 import lombok.RequiredArgsConstructor;
+import online.planner.online_planner.payload.request.DeleteAccountRequest;
 import online.planner.online_planner.payload.request.PasswordChangeRequest;
 import online.planner.online_planner.payload.request.SignUpRequest;
 import online.planner.online_planner.payload.response.UserResponse;
@@ -8,6 +9,7 @@ import online.planner.online_planner.service.user.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -47,5 +49,11 @@ public class UserController {
     @DeleteMapping("/logout")
     public void logout(@RequestHeader String deviceToken) {
         userService.logout(deviceToken);
+    }
+
+    @DeleteMapping("/account")
+    public void deleteAccount(@RequestHeader("Authorization") String token,
+                              @RequestBody DeleteAccountRequest deleteAccountRequest) {
+        userService.deleteAccount(token, deleteAccountRequest);
     }
 }
