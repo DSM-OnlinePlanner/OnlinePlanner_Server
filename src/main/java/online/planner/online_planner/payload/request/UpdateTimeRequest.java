08475f5e14e618f0a2b09e09b7a887a48bde1c98
서건희 <seogunhee4@gmail.com>
2021-09-08 15:00:10 +0900

    [FIX]

--- src/main/java/online/planner/online_planner/payload/request/UpdateTimeRequest.java
+++ src/main/java/online/planner/online_planner/payload/request/UpdateTimeRequest.java
@@ -3,14 +3,12 @@ package online.planner.online_planner.payload.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 @Getter
 public class UpdateTimeRequest {
-    @JsonFormat(pattern = "HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime startTime;
-
-    @JsonFormat(pattern = "HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime endTime;
 }
